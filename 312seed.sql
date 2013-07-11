4b8f2074a8109fb9006ef73a332f44af343bec73
brayden hancock <brayden.hancock@tvw.org>
2013-07-10 22:50:45 -0700

    Added two tables so we can have multiple logs

--- 312seed.sql
+++ 312seed.sql
@@ -12,13 +12,31 @@ DROP TABLE IF EXISTS pivots;
 CREATE TABLE pivots (
 pivot_id 		int(11) NOT NULL AUTO_INCREMENT,
 pivot_name 		char(20) NOT NULL,
-pivot_notes		varchar(5000),
+-- pivot_notes		varchar(5000),
 -- pivot_degrees 	int (3),
 -- pivot_direction char(20),
 -- pivot_status 	char(3),
 PRIMARY KEY (pivot_id)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS pivot_log;
+CREATE TABLE pivot_log (
+note_id int(11) NOT NULL AUTO_INCREMENT,
+note varchar(5000) NOT NULL,
+PRIMARY KEY (note_id)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS pivotsMap;
+CREATE TABLE pivotsMap (
+  note_id int(11) NOT NULL
+, pivot_id int(11) NOT NULL
+, id int(11) NOT NULL AUTO_INCREMENT
+, PRIMARY KEY(id)
+, KEY pivot_log_id_fk1 (note_id)
+, CONSTRAINT user_id_fk1 FOREIGN KEY (note_id) REFERENCES pivot_log (note_id)
+, KEY pivot_id_fk2 (pivot_id)
+, CONSTRAINT pivot_id_fk2 FOREIGN KEY (pivot_id) REFERENCES pivots (pivot_id))
+ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS app_users;
 CREATE TABLE app_users (
@@ -27,7 +45,6 @@ uname char(20) NOT NULL,
 uphone int(10) NOT NULL,
 PRIMARY KEY (user_id)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
 
 DROP TABLE IF EXISTS map;
 CREATE TABLE map (
@@ -45,6 +62,9 @@ ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 INSERT INTO pivots (pivot_id, pivot_name) VALUES(NULL, "Josh");
 INSERT INTO app_users VALUES(NULL, "Brayden", 2083905008);
 INSERT INTO map VALUES(1, 1, NULL);
+INSERT INTO pivot_log (note_id, note) VALUES(NULL, "Here is a note thing");
+INSERT INTO pivotsMap VALUES(1, 1, NULL);
+
 
 ALTER TABLE map
 ADD CONSTRAINT un_map UNIQUE (user_id, pivot_id);
